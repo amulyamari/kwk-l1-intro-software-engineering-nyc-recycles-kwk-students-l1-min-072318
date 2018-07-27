2fea5328246533487fa09633b300e08b842addfe
amulyamari <amulya@saimail.com>
2018-07-27 14:10:25 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,21 +1,23 @@
-###### Code Your Solutions Here!!!
-
-#Your code should work with a hash that looks like the one below (in comments).
-#The specs (which call your tests) are using these exact hashes 
-# We haven't hard coded the hashes in the tests, 
-#they're dynamically brought in from the NYC Open Data API
-
-# first_recycle_bin =  {
-#   "address"=>"E 227 St/Bronx River Pkway",
-#   "site_type"=>"Subproperty",
-#   "longitude"=>"-73.864223918",
-#   "borough"=>"Bronx",
-#   "latitude"=>"40.890848989",
-#   "park_site_name"=>"227th St. Plgd"
-# }
 
 
-#1. Define a method `address`, that takes a hash as an argument.
+first_recycle_bin =  {
+    "address"=>"E 227 St/Bronx River Pkway",
+    "site_type"=>"Subproperty",
+    "longitude"=>"-73.864223918",
+     "borough"=>"Bronx",
+     "latitude"=>"40.890848989",
+     "park_site_name"=>"227th St. Plgd"
+     }
+
+
+
+def address(first_recycle_bin)
+  return first_recycle_bin
+end 
+address()
+
+
+
 #This method should return the value of the address key in the hash.
 
 #2. Define a method called `borough` that takes a hash as an argument.
